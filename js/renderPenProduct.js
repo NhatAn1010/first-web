8cdfdcfbdfcbefbe75a73a5b29e84fb4cfe54474
NhatAn1010 <anhat1820@gmail.com>
2026-03-26 20:33:32 +0700

    Initial commit

---
+++ js/renderPenProduct.js
@@ -0,0 +1,43 @@
+export function renderPenProduct(products)
+{
+    let html = "";
+
+    products.forEach(p => {
+        html += `
+            <div class="col-6 col-md-3 col-lg-3 mb-3">
+                <div class="product card shadow">
+                    <div class="card-body">
+                        <div class="card-head mb-1 d-flex justify-content-center">
+                            <img src="${p.image}" alt="">
+                        </div>
+
+                        <div class="product-title mb-4">
+                            <a href="#"> 
+                                ${p.name}
+                            </a>
+                        </div>
+
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="text-danger">
+                                ${p.price.toLocaleString('vi-VN')}đ
+                            </div>
+                            <div class="sold fw-bold">
+                                Đã bán ${p.sold}
+                            </div>
+                        </div>
+
+                        <div class="price-sold d-flex justify-content-center">
+                            <a href="./product_detail.html?id=${p.id}&cat=pen" class="btn btn-primary">
+                                <i class="bi bi-eye"></i>
+                                Xem chi tiết
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    document.getElementById("list-product-pen").innerHTML = html;
+}
+
